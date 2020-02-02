16b8493f05bf1779cf529582c454254c02f9a03d
mohammadqaderi <mqaderi44@gmail.com>
2020-02-02 21:47:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service';
+import { log } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TDF';
+  topicHasError: boolean = true;
+  submitted = false;
+  errMsg = '';
+  topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('ali', 'ali@gmail.com' ,962785189906, 'default', '', false);
+constructor(private empService: EnrollmentService){
+
+}
+  topicValidate(value) {
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+  onSubmit(userModel) {
+    console.log(userModel);
+
+    // this.empService.enroll(this.userModel)
+    // .subscribe(
+    //   data=>{
+    //       console.log('Success',data);
+    //   },
+    //   error => this.errMsg = error.statusText
+    // )
+    // this.submitted = true;
+  }
 }
